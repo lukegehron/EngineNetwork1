4e046c3a1cc85b5353fa98a0ce2b21e08215cff7
lukegehron <luke@gehron.com>
2018-09-24 20:54:45 -0400

    Add files via upload

---
+++ Bugs.js
@@ -0,0 +1,69 @@
+let bugXArray = [];
+let bugYArray = [];
+speed = .2;
+let bugDiameter = [];
+let numBugs = 6
+
+function setup() {
+  createCanvas(710, 400);
+  for (let i=0; i<numBugs; i++){
+    let x = random(width/6, width*(5/6));
+    bugXArray.push(x);
+  }
+  for (let i=0; i<numBugs; i++){
+    let y = random(height/6, height*(5/6));
+    bugYArray.push(y);
+  }
+  for (let i=0; i<numBugs; i++){
+    let dia = random(30,100);
+    bugDiameter.push(dia);
+  }
+}
+
+function draw() {
+  background(0);
+  for (i = 0; i < numBugs; i++){
+    bugXArray[i] += random(-speed, speed);
+    bugYArray[i] += random(-speed, speed);
+    noFill();
+    stroke(255);
+    //ellipse(bugXArray[i], bugYArray[i], bugDiameter[i], bugDiameter[i]);
+  }
+  for (let i = 0; i < numBugs; i++){
+    for (let j = 0; j < numBugs; j++){
+      stroke((j+10)*10,(i+5)*20,(j+10)*25);
+    //line(bugXArray[i],bugYArray[i],bugXArray[j],bugYArray[j]);
+    let line1X = 0;
+    let line1Y = 0;
+    let line2X = 0;
+    let line2Y = 0;
+    if (bugDiameter[i] < 50 && bugDiameter[j] < 50){
+      line1X = bugDiameter[i]/2*sin((frameCount/bugDiameter[i])+bugDiameter[i])+bugXArray[i];
+      line1Y = bugDiameter[i]/2*cos((frameCount/bugDiameter[i])+bugDiameter[i])+bugYArray[i];
+      line2X = bugDiameter[j]/2*sin((frameCount/bugDiameter[j])+bugDiameter[j])+bugXArray[j];
+      line2Y = bugDiameter[j]/2*cos((frameCount/bugDiameter[j])+bugDiameter[j])+bugYArray[j];
+    }else if  (bugDiameter[i] > 50 && bugDiameter[j] < 50){
+      line1X = bugDiameter[i]/2*sin((-frameCount/bugDiameter[i])+bugDiameter[i])+bugXArray[i];
+      line1Y = bugDiameter[i]/2*cos((-frameCount/bugDiameter[i])+bugDiameter[i])+bugYArray[i];
+      line2X = bugDiameter[j]/2*sin((frameCount/bugDiameter[j])+bugDiameter[j])+bugXArray[j];
+      line2Y = bugDiameter[j]/2*cos((frameCount/bugDiameter[j])+bugDiameter[j])+bugYArray[j];
+    }else if  (bugDiameter[j] > 50 && bugDiameter[i] < 50){
+      line1X = bugDiameter[i]/2*sin((frameCount/bugDiameter[i])+bugDiameter[i])+bugXArray[i];
+      line1Y = bugDiameter[i]/2*cos((frameCount/bugDiameter[i])+bugDiameter[i])+bugYArray[i];
+      line2X = bugDiameter[j]/2*sin((-frameCount/bugDiameter[j])+bugDiameter[j])+bugXArray[j];
+      line2Y = bugDiameter[j]/2*cos((-frameCount/bugDiameter[j])+bugDiameter[j])+bugYArray[j];
+    }else{
+      line1X = bugDiameter[i]/2*sin((-frameCount/bugDiameter[i])+bugDiameter[i])+bugXArray[i];
+      line1Y = bugDiameter[i]/2*cos((-frameCount/bugDiameter[i])+bugDiameter[i])+bugYArray[i];
+      line2X = bugDiameter[j]/2*sin((-frameCount/bugDiameter[j])+bugDiameter[j])+bugXArray[j];
+      line2Y = bugDiameter[j]/2*cos((-frameCount/bugDiameter[j])+bugDiameter[j])+bugYArray[j];
+    }
+    line(line1X,line1Y,line2X,line2Y);
+    fill(255);
+    ellipse(line1X,line1Y,5,5);
+    if (mouseX < line1X+15 && mouseX > line1X - 15 && mouseY < line1Y+15 && mouseY > line1Y - 15 ){
+      text("ENGINE", line1X, line1Y);
+    }
+  }
+ }
+}
